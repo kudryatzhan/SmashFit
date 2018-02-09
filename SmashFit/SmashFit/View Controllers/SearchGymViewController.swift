f03e60e80df6ddf19bd3e5e05aaa63851049e0fe
Kudryatzhan Arziyev <karziyev@gmail.com>
2018-02-08 20:06:43 -0700

    Added searchGymVC, addGymVC

---
+++ SmashFit/SmashFit/View Controllers/SearchGymViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PickGymViewController.swift
+//  SmashFit
+//
+//  Created by Kudryatzhan Arziyev on 2/8/18.
+//  Copyright © 2018 Kudryatzhan Arziyev. All rights reserved.
+//
+
+import UIKit
+
+class SearchGymViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var gymSearchBar: UISearchBar!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
